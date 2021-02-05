81c243226e6af7d3fc0417f6af6cc58061d792e1
LSOffice <unconfigured@null.spigotmc.org>
2021-02-05 15:59:40 +0800

    v0.0.2

--- main/scene/scene.py
+++ main/scene/scene.py
@@ -7,6 +7,7 @@ class StartScene(SceneBase):
     def __init__(self, screen):
         SceneBase.__init__(self)
         self.screen = screen
+        self.button1 = pygame.Rect(100, 100, screen.get_width()-200, screen.get_height()-200)
 
     def ProcessInput(self, events, pressed_keys):
         for event in events:
@@ -17,4 +18,5 @@ class StartScene(SceneBase):
 
     def Render(self, screen):
         screen.fill((255, 255, 255))
+        pygame.draw.rect(screen, (0, 0, 0), self.button1)
         pygame.display.flip()
